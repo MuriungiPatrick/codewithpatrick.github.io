b4771573f1a2cab3945cf238476883c4464af5ec
Patrick Muriungi <muriungipatrick@gmail.com>
2022-11-24 14:44:28 +0300

    Initial Commit

---
+++ public/js/dark-mode.js
@@ -0,0 +1,13 @@
+
+
+function addDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");   
+}
+
+
+if(document.body.element.classList.contains("dark-mode")) {
+    button.src = "public/images/sun.svg";
+   } else  {
+    button.src = "public/images/moon-fill.svg";
+}
